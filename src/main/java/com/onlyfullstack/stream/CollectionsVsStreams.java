1bd7da26ba4a2643dc8553bb51321830e2d3a000
onlyfullstack <onlyfullstackdev@gmail.com>
2020-05-17 12:28:05 +0100

    Collections Vs Streams

--- src/main/java/com/onlyfullstack/stream/CollectionsVsStreams.java
+++ src/main/java/com/onlyfullstack/stream/CollectionsVsStreams.java
@@ -1,4 +1,20 @@
 package com.onlyfullstack.stream;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CollectionsVsStreams {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList(new String[] {"only", "fullstack", "java", "spring", "automation"}));
+
+        System.out.println("Collection Printing");
+        for(String names : list) {
+            System.out.println(names);
+        }
+
+        System.out.println("\n\nStream Printing");
+        list.stream().forEach(System.out::println);
+    }
 }
